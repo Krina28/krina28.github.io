b07ad37d58f7cee4baf89cd388d420598d7be43a
Krina Soni <krina.soni@openxcellinc.com>
2018-11-23 10:14:12 +0530

    Blogs & Photograph module

--- js/script.js
+++ js/script.js
@@ -10,3 +10,16 @@ typewriter.typeString('Ingenious Creature')
     .pauseFor(2500)
     .deleteAll()
     .start();
+
+var elem = document.querySelector('.grid');
+var msnry = new Masonry(elem, {
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: 200
+});
+
+// element argument can be a selector string
+//   for an individual element
+var msnry = new Masonry('.grid', {
+    // options
+});
